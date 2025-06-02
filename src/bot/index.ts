33070f9975d47d5b8f2c5f8314a483a702117cc1
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-06-02 12:54:55 +0200

    fix issue with command runtime

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -7,7 +7,10 @@ import { load_all } from "./load_all";
 export const config = cfg;
 
 export const bot = new Client({
-    intents: [32767],
+    intents: [
+        1048576, 2097152, 8192, 4096, 16384, 8, 16, 64, 2, 1024, 512, 2048, 4, 256, 65536,
+        1, 128, 32, 32768, 16777216, 33554432
+    ],
     allowedMentions: {
         repliedUser: false,
         roles: [],
--- src/events/on_message.ts
+++ src/events/on_message.ts
@@ -6,11 +6,10 @@ export const new_message: event_type = {
     name: "messageCreate",
     once: false,
     function(client, x: Message) {
-        console.log(x.content)
         if (!x.content.startsWith(config.DEFAULT_BOT_PREFIX)) { return; }
         if (x.channel.type !== ChannelType.GuildText) return;
 
-        if (x.guild?.members.me?.permissions.has(PermissionFlagsBits.Administrator)) return;
+        if (!x.guild?.members.me?.permissions.has(PermissionFlagsBits.Administrator)) return;
 
         let args = x.content.slice(config.DEFAULT_BOT_PREFIX.length).trim().split(/ +/g);
         let cmd = client.commands.get(args.shift() || "");
